e88e4a9f87d0139e82115430869109418ac16ee9
Asit0009 <asitpawar7@gmail.com>
2026-04-23 02:19:02 +0530

    Added CI/CD pipeline with Docker and ML model

---
+++ model.py
@@ -0,0 +1,20 @@
+import pickle
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+
+# Load data
+X, y = load_iris(return_X_y=True)
+
+# Split
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+# Train
+model = RandomForestClassifier()
+model.fit(X_train, y_train)
+
+# Save model
+with open("model.pkl", "wb") as f:
+    pickle.dump(model, f)
+
+print("Model trained and saved!")
